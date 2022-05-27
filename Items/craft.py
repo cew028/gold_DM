1a660439c48ad9174b574a5d7196a1bb49d05acd
Eric <coffeenathaneric@yahoo.com>
2022-05-27 18:37:26 -0500

    Dropping and picking up items

--- Items/craft.py
+++ Items/craft.py
@@ -6,11 +6,21 @@ class Item():
         damage_die = 0,
         armor_bonus = 0,
         effect = "No effect",
-        
-        
+        wieldable = False,
+        wearable_on_head = False,
+        wearable_on_torso = False,
+        wearable_on_legs = False,
+        wearable_on_feet = False,
+        wearable_on_hands = False,
     ):
         self.name = name
         self.description = description
         self.damage_die = damage_die
         self.armor_bonus = armor_bonus
         self.effect = effect
+        self.wieldable = wieldable
+        self.wearable_on_head = wearable_on_head
+        self.wearable_on_torso = wearable_on_torso
+        self.wearable_on_legs = wearable_on_legs
+        self.wearable_on_feet = wearable_on_feet
+        self.wearable_on_hands = wearable_on_hands
--- engine.py
+++ engine.py
@@ -22,10 +22,10 @@ def gameplay_loop():
             question = "What do you do?",            
             # Since prompt.open_response selects the last valid match, make sure more common options come after less common ones.
             options = [
-                "examine",
-                "look",
-                "go to",
-                "go",
+                "drop",
+                "take", "pick up", "grab",
+                "examine", "look",
+                "go to", "go",
             ],
         )
         match what_you_do:
@@ -33,6 +33,10 @@ def gameplay_loop():
                 go_to()
             case "examine" | "look":
                 look_at()
+            case "take" | "pick up" | "grab":
+                pick_up()
+            case "drop":
+                drop()
 
 def go_to():
     """Ask the player where they want to go. Then change their location to be the destination."""
@@ -40,8 +44,7 @@ def go_to():
         question = "Where do you want to go?",
         options = [room.name for room in you.location.accessible_rooms],
     )
-    destination = match_string_to_class(target, you.location.accessible_rooms)
-    you.location = destination
+    you.location = match_string_to_class(target, you.location.accessible_rooms)
     examine(you.location)
 
 def look_at():
@@ -58,7 +61,7 @@ def look_at():
     the_items_in_the_room = [item.name for item in you.location.contents]
     target = prompt.open_response(
         question = "What do you want to look at?",
-        options = themselves + the_room + their_items + the_items_in_the_room
+        options = themselves + the_room + their_items + the_items_in_the_room,
     )
     if target in themselves:
         you.character_sheet()
@@ -79,3 +82,29 @@ def match_string_to_class(string, list):
             output = object
             break
     return output
+
+def pick_up():
+    """The player takes an item from the room and adds it to their inventory."""
+    target = prompt.open_response(
+        question = "What do you want to pick up?",
+        options = [item.name for item in you.location.contents],
+    )
+    item = match_string_to_class(target, you.location.contents)
+    you.location.contents.remove(item)
+    you.inventory.append(item)
+    print(f"\nYou pick up the {item.name}.")
+
+def drop():
+    """The player chooses an item from their inventory and "drops it;" i.e., adds it to the room's contents."""
+    list_of_items = []
+    if you.inventory != []:
+        list_of_items = [item.name for item in you.inventory]
+    target = prompt.multiple_choice(
+        question = "What do you want to drop?",
+        options = ["Nothing"] + list_of_items,
+    )
+    if target != "Nothing":
+        item = match_string_to_class(target, you.inventory)
+        you.inventory.remove(item)
+        you.location.contents.append(item)
+        print(f"\nYou drop the {item.name}.")
